66fc4e91d90c307f4e5d350f8ca85b41e0d12507
wagner maidson <wagner.rmaidson@gmail.com>
2024-12-27 14:29:45 -0300

    remove dotenv

--- src/main/java/com/tomangg/TomanggApplication.java
+++ src/main/java/com/tomangg/TomanggApplication.java
@@ -1,15 +1,10 @@
 package com.tomangg;
-
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TomanggApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
         SpringApplication.run(TomanggApplication.class, args);
     }
 }
